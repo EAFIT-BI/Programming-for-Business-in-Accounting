647efa4639d97e49f86ea2e048962130e14ede26
Lina María Sepúlveda Cano <lmsepulvedaca@gmail.com>
2024-02-05 13:13:55 -0500

    Add files via upload

---
+++ simulacro.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Creación de un DataFrame con 3 características Nombre, Edad, Ciudad. y 5 filas
+de registros con la información solicitada. El DataFrame se debe llamar
+datos
+"""
+
+def crear():
+    #crea aquí tu código
+    return datos
+
